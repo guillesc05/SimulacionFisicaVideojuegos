5655f8c0f11b4c0be6cdfd899688705e595d29a2
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-11-03 17:20:05 +0100

    sistemas

--- skeleton/Scenes/Scene2.cpp
+++ skeleton/Scenes/Scene2.cpp
@@ -15,23 +15,23 @@ void Scene2::start() {
 	particleSystem->addParticleGenerator(new UniformParticleGenerator(physx::PxVec3(100, 0, 0), physx::PxVec3(0), 5, 5, 50, 1, .1, 3));
 	particleSystem->addParticleGenerator(new GaussianParticleGenerator(physx::PxVec3(-100, 0, 0), physx::PxVec3(0), 5, 5, 50, 1, .1, 3));
 
-	//auto gravitationalForce = new GravitationalForceGenerator(physx::PxVec3(0.f, -9.8f, 0.f), 1.);
-	//auto whirlwindForceGenerator = new WhirlwindForceGenerator(1, 1, 0, physx::PxVec3(100));
+	auto gravitationalForce = new GravitationalForceGenerator(physx::PxVec3(0.f, -9.8f, 0.f), 1.);
+	auto whirlwindForceGenerator = new WhirlwindForceGenerator(1, 1, 0, physx::PxVec3(100));
 	auto explosion = new ExplosionForceGenerator(physx::PxVec3(0, 0, 0), 100, 1, 10000);
 
-	//updateableObjects.push_back(gravitationalForce);
-	//updateableObjects.push_back(whirlwindForceGenerator);
+	updateableObjects.push_back(gravitationalForce);
+	updateableObjects.push_back(whirlwindForceGenerator);
 	updateableObjects.push_back(explosion);
 
-	//particleSystem->addForceGenerator(gravitationalForce);
+	particleSystem->addForceGenerator(gravitationalForce);
 	//particleSystem->addForceGenerator(new WindForceGenerator(physx::PxVec3(1.f, 0.f, 0.f), 100, 1, 0, physx::PxVec3(0), 200));
-	//particleSystem->addForceGenerator(whirlwindForceGenerator);
+	particleSystem->addForceGenerator(whirlwindForceGenerator);
 	particleSystem->addForceGenerator(explosion);
 }
 
 void Scene2::keyPress(unsigned char key, const physx::PxTransform& camera) {
 	if (key == 'g') {
-		auto explosion = new ExplosionForceGenerator(physx::PxVec3(0, 0, 0), 100, 1, 10000);
+		auto explosion = new ExplosionForceGenerator(physx::PxVec3(0, 0, 0), 100, 1, 100000);
 		updateableObjects.push_back(explosion);
 		particleSystem->addForceGenerator(explosion);
 	}
