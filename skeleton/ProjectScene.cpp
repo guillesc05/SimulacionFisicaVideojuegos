bee35c177262bbfb27cb340ce7c431372c01a20c
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-11-04 19:23:09 +0100

    a

--- skeleton/ProjectScene.cpp
+++ skeleton/ProjectScene.cpp
@@ -9,7 +9,7 @@ void ProjectScene::start() {
 	GaussianParticleGenerator* cloudParticles = new GaussianParticleGenerator(physx::PxVec3(0, 0, 0), physx::PxVec3(0), 5, 5, 150, 1, .1, 3);
 	pSystem->addParticleGenerator(cloudParticles);
 
-	GaussianParticleGenerator* engineParticles = new GaussianParticleGenerator(physx::PxVec3(0, 0, 0), physx::PxVec3(0, 0, 1), 2, 200, 0, 0.05, .1, 30);
+	GaussianParticleGenerator* engineParticles = new GaussianParticleGenerator(physx::PxVec3(0, 0, 0), physx::PxVec3(0, 0, 1), 2, 200, 0, 0.05, .1, 15);
 	pSystem->addParticleGenerator(engineParticles);
 
 	_spaceShip = new Spaceship(physx::PxVec3(0),engineParticles);
