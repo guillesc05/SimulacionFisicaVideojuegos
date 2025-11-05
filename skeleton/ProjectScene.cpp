92d0f75773fad9d70f58c0b070d91b0efc730f2d
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-11-05 11:03:38 +0100

    tolbellino

--- skeleton/ProjectScene.cpp
+++ skeleton/ProjectScene.cpp
@@ -1,12 +1,13 @@
 #include "ProjectScene.h"
 #include "Spaceship.h"
 #include "ParticleSystem.h"
+#include "WhirlwindForceGenerator.h"
 
 void ProjectScene::start() {
-	ParticleSystem* pSystem = new ParticleSystem(6);
+	ParticleSystem* pSystem = new ParticleSystem(10);
 	updateableObjects.push_back(pSystem);
 
-	GaussianParticleGenerator* cloudParticles = new GaussianParticleGenerator(physx::PxVec3(0, 0, 0), physx::PxVec3(0), 5, 5, 150, 1, .1, 3);
+	GaussianParticleGenerator* cloudParticles = new GaussianParticleGenerator(physx::PxVec3(0, 0, 0), physx::PxVec3(0), 5, 5, 250, 1, .1, 3);
 	pSystem->addParticleGenerator(cloudParticles);
 
 	GaussianParticleGenerator* engineParticles = new GaussianParticleGenerator(physx::PxVec3(0, 0, 0), physx::PxVec3(0, 0, 1), 2, 200, 0, 0.05, .1, 15);
@@ -15,6 +16,10 @@ void ProjectScene::start() {
 	_spaceShip = new Spaceship(physx::PxVec3(0),engineParticles);
 	_spaceShip->setDamping(0.5);
 	updateableObjects.push_back(_spaceShip);
+
+	auto whirlwindForceGenerator = new WhirlwindForceGenerator(.5, 1, 0, physx::PxVec3(0, -100, 0));
+	updateableObjects.push_back(whirlwindForceGenerator);
+	pSystem->addForceGenerator(whirlwindForceGenerator);
 }
 
 void ProjectScene::keyPress(unsigned char key, const physx::PxTransform& camera) {
